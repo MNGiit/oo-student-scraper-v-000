f76dd5b6a6c4e6812782e4498a7e203f1f091914
Mohammed Noori <kmanoorib@gmail.com>
2018-07-30 21:02:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -74,7 +74,7 @@ class Scraper
     #doc.css(".social-icon-container").children.css("a").map { |item| item.attribute('href').value}
     #long way of writing this
     #REMEMBER .map is the same as .collect
-    doc.css(".social-icon-container").children.css("a").collect |item|
+    doc.css(".social-icon-container").children.css("a").each.map |item|
      item.attribute('href').value
     end
   end
