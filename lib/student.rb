d4bb736191b6b8b88a604fc80ae5fb883e71210d
Mohammed Noori <kmanoorib@gmail.com>
2018-07-30 23:34:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {|student| Scraper.scrape_index_page(student)}
+    students_array.each {|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
