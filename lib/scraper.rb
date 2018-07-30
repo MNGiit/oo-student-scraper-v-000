7af2e54219bca2b54e18b6c516a5b2dabc878a01
Mohammed Noori <kmanoorib@gmail.com>
2018-07-30 20:41:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,21 +17,56 @@ class Scraper
     cards = items.css("div.student-card")
     #cards = items.css(".student-card")
     
-    card_name = cards[0].css('.student-name').text #this works for name
-    card_location = cards[0].css(".student-location").text #this works for location
-    #card_url = cards[0].css("#{.attr('href')}")
+    #student_name = cards[0].css(".student-name").text #this works, now do it for all of them
     
-    items.each do |item|
-      card_name = item.css(".student-name").text
-      card_location = item.css(".student-location").text
-      students << {name: card_name, location: card_location}
+    cards.each do |card|
+      card_name = card.css(".student-name").text
+      card_location = card.css(".student-location").text
+      card_url = card.css("a")[0].attributes["href"].value
+      students << {name: card_name, location: card_location, profile_url: card_url}
     end
     students
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
+    #student = {twitter: "", linkedin: "", github: "", blog: "", profile_quote: "", bio: ""}
+    student = {}
     
+    html = File.read(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    scraped_links = doc.css(".social-icon-container").children.css("a")
+    binding.pry
+    student[:twitter] = doc.css(".social-icon-container").children.css("a").attribute("href").value
+    #scraped_twitter = doc.css(".social-icon-container").children.css("a").attribute("href").value
+    
+    student[:linkedin] = doc.css(".social-icon-container").children.css("a")[1].attribute("href").value
+    #scraped_linkedin = doc.css(".social-icon-container").children.css("a")[1].attribute("href").value
+    
+    if doc.css(".social-icon-container").children.css("a")[2]
+      student[:github] = doc.css(".social-icon-container").children.css("a")[2].attribute("href").value
+    end
+    #scraped_github = doc.css(".social-icon-container").children.css("a")[2].attribute("href").value
+    
+    if doc.css(".social-icon-container").children.css("a")[3]
+      student[:blog] = doc.css(".social-icon-container").children.css("a")[3].attribute("href").value
+    end
+    #scraped_blog = doc.css(".social-icon-container").children.css("a")[3].attribute("href").value
+    
+    student[:profile_quote] = doc.css(".profile-quote").text if doc.css(".profile-quote")
+    #scraped_profile_quote = doc.css(".profile-quote").text #if doc.css(".profile-quote")
+    
+    if doc.css("div.bio-content.content-holder div.description-holder p").text
+      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+    end
+    #student[:bio] = doc.css(".description-holder").text #if doc.css(".description-holder").text
+    #scraped_bio = doc.css(".description-holder").text #if doc.css(".description-holder").text
+    
+    student
+    #binding.pry
+    
+    #student = {twitter: scraped_twitter, linkedin: scraped_linkedin, github: scraped_github, blog: scraped_blog, profile_quote: scraped_profile_quote, bio: scraped_bio}
+    #binding.pry
   end
 
 end
